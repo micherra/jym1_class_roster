0c52e3ed5b8b189f7bd1a6159bf576c4e6c213c0
Michelle Herrera <mher754@wgu.edu>
2023-01-26 20:18:25 -0500

    Implantation of 80% of Student functionality

--- jym1_class_roster/student.cpp
+++ jym1_class_roster/student.cpp
@@ -8,6 +8,7 @@ using namespace std;
 // TODO: Student::setDegreeProgram
 // TODO: Student::getDaysToCompleteCourses
 // TODO: Student::getDegreeProgram
+// TODO: Student::print
 
 void Student::setId(string studentId) {
     this->studentId = studentId;
@@ -18,12 +19,37 @@ void Student::setFirstName(string firstName) {
 }
 
 
-//void Student::setLastName(string lastName);
-//void Student::setEmailAddress(string email);
-//void Student::setAge(int age);
-//void Student::print(Student student);
-//string Student::getId();
-//string Student::getFirstName();
-//string Student::getLastName();
-//string Student::getEmailAddress();
-//int Student::getAge();
+void Student::setLastName(string lastName) {
+    this->lastName = lastName;
+}
+
+void Student::setEmailAddress(string emailAddress) {
+    this->emailAddress = emailAddress;
+}
+void Student::setAge(int age) {
+    this->age = age;
+}
+
+//void Student::print(Student student) {
+//
+//}
+
+string Student::getId() {
+    return this->studentId;
+}
+
+string Student::getFirstName() {
+    return this->firstName;
+}
+
+string Student::getLastName() {
+    return this->lastName;
+}
+
+string Student::getEmailAddress() {
+    return this->emailAddress;
+}
+
+int Student::getAge() {
+    return this->age;
+}

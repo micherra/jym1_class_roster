4f0faaa806ea47340c7591e7fdca3725cf303e48
Michelle Herrera <mher754@wgu.edu>
2023-01-29 17:05:17 -0500

    Updated tests and clearer differentiation in test runner

--- jym1_class_roster/tests.cpp
+++ jym1_class_roster/tests.cpp
@@ -1,16 +1,25 @@
+#define RESET   "\033[0m"
+#define RED     "\033[1m\033[31m" 
+#define GREEN   "\033[1m\033[32m"
+#define PASSED  "PASSED"
+#define FAILED  "FAILED"
+
 #include "tests.hpp"
+
 #include <iostream>
 using namespace std;
 
 void TestRunner::expect(string testCase, bool result) {
-  string const status = result ? "PASSED" : "FAILED";
-  cout << testCase << boolalpha << status << endl;
+  string const color = result ? GREEN : RED;
+  string const status = result ? PASSED : FAILED;
+  cout << testCase << boolalpha << color << status << RESET << endl;
 }
 
 void TestRunner::testStudent() {
   Student student = this -> student;
   
   cout << "Running Student Test Suite\n";
+  
   expect("Gets student's id => ", student.getId() == "A1");
   
   expect("Gets first name of student => ", student.getFirstName() == "John");
@@ -21,7 +30,13 @@ void TestRunner::testStudent() {
   
   expect("Gets student's age => ", student.getAge() == 20);
   
-  expect("Gets student's degree program => ", student.getDegreeProgram() == SECURITY);
+  expect("Gets student's days to complete three courses",
+    student.getDaysToCompleteCourses().at(0) == 30 &
+    student.getDaysToCompleteCourses().at(1) == 35 &
+    student.getDaysToCompleteCourses().at(2) == 40
+  );
+  
+  expect("Gets student's degree program => ", student.getDegreeProgram() == "Security");
   
   student.setId("A2");
   expect("Sets student's id => ", student.getId() == "A2");
@@ -38,6 +53,13 @@ void TestRunner::testStudent() {
   student.setAge(25);
   expect("Sets student's age => ", student.getAge() == 25);
   
+  student.setDaysToCompleteCourses(35, 40, 45);
+  expect("Sets student's age => ",
+    student.getDaysToCompleteCourses().at(0) == 35 &
+    student.getDaysToCompleteCourses().at(1) == 40 &
+    student.getDaysToCompleteCourses().at(2) == 45
+  );
+  
   student.setDegreeProgram(NETWORK);
-  expect("Sets student's degree program => ", student.getDegreeProgram() == NETWORK);
+  expect("Sets student's degree program => ", student.getDegreeProgram() == "Network");
 }
--- jym1_class_roster/tests.hpp
+++ jym1_class_roster/tests.hpp
@@ -9,10 +9,14 @@ using namespace std;
 
 class TestRunner {
 private:
-    void expect(string testCase, bool result);
-    const Student student =  Student("A1", "John", "Smith", "John1989@gm", 20, 30, 35, 40, SECURITY);;
+  void expect(string testCase, bool result);
+  
+  const Student student = Student("A1", "John", "Smith", "John1989@gm", 20, {30, 35, 40}, SECURITY);
+  
+  const string testOutput = "A1/tFirst Name: John/tLast Name: Smith/tAge: 20 /tdaysInCourse: {35, 40, 55}/tDegree Program: Security";
+  
 public:
-    void testStudent();
+  void testStudent();
 };
 
 #endif

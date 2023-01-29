a62d582e63cbde91b34e3cd736fb9d2f4ef4245e
Michelle Herrera <mher754@wgu.edu>
2023-01-29 17:02:42 -0500

    Fixed typo  and smaller clean up

--- jym1_class_roster/degree.hpp
+++ jym1_class_roster/degree.hpp
@@ -1,8 +1,11 @@
 #ifndef degree_hpp
 #define degree_hpp
 
+#include <string>
+#include <array>
+using namespace::std;
+
 /// Types of Degree Programs
 enum DegreeProgram { SECURITY, NETWORK, SOFTWARE };
-const char* degree_program[] = { "Security", "Network", "Software" };
-
+static const char *degreeName[] = { "Security", "Network", "Software" };
 #endif

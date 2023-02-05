ff59d74e613c8ad78e7b192f493cb4219fd01883
Michelle Herrera <mher754@wgu.edu>
2023-02-04 23:35:18 -0500

    Added functionality to convert an string to an enum

--- jym1_class_roster/degree.hpp
+++ jym1_class_roster/degree.hpp
@@ -1,11 +1,31 @@
 #ifndef degree_hpp
 #define degree_hpp
 
+#include <map>
 #include <string>
 #include <array>
 using namespace::std;
 
 /// Types of Degree Programs
 enum DegreeProgram { SECURITY, NETWORK, SOFTWARE };
+
 static const char *degreeName[] = { "Security", "Network", "Software" };
+
+static const map<string, DegreeProgram> degreeNameMap {
+  { "SECURITY", SECURITY },
+  { "NETWORK", NETWORK },
+  { "SOFTWARE", SOFTWARE }
+};
+
+static const DegreeProgram toEnum(string degreeName) {
+  // Mutates the provided argument.
+  transform(
+    degreeName.begin(),
+    degreeName.end(),
+    degreeName.begin(),
+    [](unsigned char c) { return toupper(c); }
+  );
+  return degreeNameMap.find(degreeName)->second;
+}
+
 #endif

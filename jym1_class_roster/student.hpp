1e7952720df5086e1143ad7097d53b03c2416bbf
Michelle Herrera <mher754@wgu.edu>
2023-02-04 23:41:42 -0500

    ToString for enum definition. Default constructor. Formatting

--- jym1_class_roster/student.hpp
+++ jym1_class_roster/student.hpp
@@ -18,11 +18,21 @@ private:
   vector<unsigned int> daysToCompleteCourses_;
   DegreeProgram degreeProgram_;
   
- const string toString_(vector<unsigned int> vector) const;
- const string toString_(unsigned int num) const;
+  const string toString_(vector<unsigned int> vector) const;
+  const string toString_(unsigned int num) const;
+  const string toString_(DegreeProgram degreeProgram) const;
   
 public:
   /// Member initialization
+  Student() :
+  studentId_(""),
+  firstName_(""),
+  lastName_(""),
+  emailAddress_(""),
+  age_(0),
+  daysToCompleteCourses_(vector<unsigned int>()),
+  degreeProgram_(NETWORK) {}
+  
   Student(
     const string &studentId,
     const string &firstName,
@@ -43,31 +53,31 @@ public:
   /**
    Sets studentId for the class instance.
    @parameter studentId: Student identification
-   **/
+   */
   void setId(string studentId);
   
   /**
    Sets firstName for the class instance.
    @parameter firstName: First name of student
-   **/
+   */
   void setFirstName(string firstName);
   
   /**
    Sets lastName for the class instance.
    @parameter lastName: Last name of student
-   **/
+   */
   void setLastName(string lastName);
   
   /**
    Sets emailAddress for the class instance.
    @parameter email: Email address associated to student
-   **/
+   */
   void setEmailAddress(string email);
   
   /**
    Sets age for the class instance.
    @parameter age: Age of the student
-   **/
+   */
   void setAge(unsigned int age);
   
   /**
@@ -75,7 +85,7 @@ public:
    @parameter daysInCourse1: Numeric days spent in course
    @parameter daysInCourse2: Numeric days spent in course
    @parameter daysInCourse3: Numeric days spent in course
-   **/
+   */
   void setDaysToCompleteCourses(
     unsigned int daysInCourse1,
     unsigned int daysInCourse2,
@@ -85,60 +95,60 @@ public:
   /**
    Sets degree program for the class instance.
    @parameter degreeProgram: Degree program associated to student
-   **/
+   */
   void setDegreeProgram(DegreeProgram degreeProgram);
   
   /**
    Get student Id.
    @returns studentId: Student identification
-   **/
+   */
   const string getId() const;
   
   /**
    Gets student's first name.
    @returns firstName: First name of student
-   **/
+   */
   const string getFirstName() const;
   
   /**
    Gets student's last name.
    @returns lastName: Last name of student
-   **/
+   */
   const string getLastName() const;
   
   /**
    Gets student's email address.
    @returns email: Email address associated to student
-   **/
+   */
   const string getEmailAddress() const;
   
   /**
    Gets student's age.
    @returns age: Age of the student
-   **/
+   */
   const unsigned int getAge() const;
   
   /**
    Gets student's days to complete three courses.
    @returns daysToCompleteCourses: Student's days to complete three courses
-   **/
+   */
   const vector<unsigned int> getDaysToCompleteCourses() const;
   
   /**
-   Gets student's degree program.
+   Gets student's degree program as formatted string.
    @returns degreeProgram: Student's degree program
-   **/
+   */
   const string getDegreeProgram() const;
-
+  
   /**
    Helper function for print.
    @returns Description of student
-   **/
+   */
   const string getMessage() const;
   
   /**
    Prints a tab seperated string of the Student's data.
-   **/
+   */
   const void print() const;
 };
 
